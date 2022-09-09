9d3b719e2eed88d2ccc6ee63c3fdfc0bb1622c27
Lee-JungBum <75216723+Lee-JungBum@users.noreply.github.com>
2022-09-09 17:58:18 +0900

    Update Product.js

--- src/js/Product.js
+++ src/js/Product.js
@@ -16,8 +16,7 @@ const Product = () => {
       e.preventDefault()
       try {
         await axios
-         .post("https://5d2e86fa-7992-4716-992a-7f1300e8c748.mock.pstmn.io/review", {
-          // .post("http://58.124.240.242:9876/client",{
+         .post("", {
             email: this.userEmail,
             //token: token,
           })
